3a75ed0850aa532ea17a4e4e5852503f745b4274
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-01-20 11:08:11 +0800

    crm update

--- src/components/Crm/CrmIndex.vue
+++ src/components/Crm/CrmIndex.vue
@@ -1,67 +1,59 @@
 <template>
   <div>
-    <div id="biaoti">
+    <div id="biaoti" >
       每日信动SCRM一站式客户运营管理平台
     </div>
-    <div style="width: 90%; margin: 0% 5%; height: 60px;">
+    <div style="width: 90%; margin: 5% 5% 0% 5%; height: 60px;">
       <div class="gongneng" style="float: left;margin-left: 0%;">市场获客提升</div>
       <div class="gongneng">营销转化增长</div>
       <div class="gongneng" style="margin-right: 0;float: right;">高效便捷运营</div>
     </div>
     <div style="margin: 2% 5%;width: 90%;height: 260px;">
-      <div class="gezi">
-        <div class="shangfang">
-          <div class="zuofu">
-            <p class="wenan2">寻找目标</p>
-          </div>
-          <div class='wenan'>海量企业库</div>
-        </div>
-        <div class="youbian">》</div>
-      </div>
-      <div class="gezi">
-        <div class="shangfang">
-          <div class="zuofu">
-            <p class="wenan2">行业聚焦</p>
-          </div>
-          <div class='wenan'>行业四级分类</div>
-        </div>
-        <div class="youbian">》</div>
-      </div>
-      <div class="gezi">
-        <div class="shangfang">
-          <div class="zuofu">
-            <p class="wenan2">销售分配</p>
-          </div>
-          <div class='wenan'>准确匹配销售</div>
-        </div>
-        <div class="youbian">》</div>
-      </div>
-      <div class="gezi">
-        <div class="shangfang">
-          <div class="zuofu">
-            <p class="wenan2">线索公海</p>
-          </div>
-          <div class='wenan'>精准线索挖掘</div>
-        </div>
-        <div class="youbian">》</div>
-      </div>
-      <div class="gezi">
-        <div class="shangfang">
-          <div class="zuofu">
-            <p class="wenan2">深度转化</p>
-          </div>
-          <div class='wenan'>高粘性复合产品</div>
-        </div>
-        <div class="youbian">》</div>
-      </div>
-      <div class="gezi" style="width: 11%;float: right;">
-        <div class="shangfang" style="width: 150px;">
-          <div class="zuofu">
-            <p class="wenan2">成单复购</p>
-          </div>
-          <div class='wenan'>其它精细运营</div>
-        </div>
-      </div>
+      <table style="width: 100%">
+        <tr>
+          <td class = 'bigtd'>
+            <div class="zuofu" >
+              <p class="wenan2">寻找目标</p>
+            </div>
+            <div class = 'wenan'>海量企业库</div>
+          </td>
+          <td class="smalltd"><div class="youbian" >》 </div></td>
+          <td class = 'bigtd'>
+            <div class="zuofu" >
+              <p class="wenan2">行业聚焦</p>
+            </div>
+            <div class = 'wenan'>行业四级分类</div>
+          </td>
+          <td class="smalltd"><div class="youbian" >》 </div></td>
+          <td class = 'bigtd'>
+            <div class="zuofu" >
+              <p class="wenan2">销售分配</p>
+            </div>
+            <div class = 'wenan'>准确匹配销售</div>
+          </td>
+          <td class="smalltd"><div class="youbian" >》 </div></td>
+          <td class = 'bigtd'>
+            <div class="zuofu" >
+              <p class="wenan2">线索公海</p>
+            </div>
+            <div class = 'wenan'>精准线索挖掘</div>
+          </td>
+          <td class="smalltd"><div class="youbian" >》 </div></td>
+          <td class = 'bigtd'>
+            <div class="zuofu" >
+              <p class="wenan2" >深度转化</p>
+            </div>
+            <div class = 'wenan'>高粘性复合产品</div>
+          </td>
+          <td class="smalltd"><div class="youbian" >》 </div></td>
+          <td class = 'bigtd'>
+            <div class="zuofu" >
+              <p class="wenan2" >成单复购</p>
+            </div>
+            <div class = 'wenan'>其它精细运营</div>
+          </td>
+        </tr>
+      </table>
     </div>
     <div class="footwenan">敬请期待</div>
   </div>
@@ -69,55 +61,23 @@
 
 <script>
 export default {
-  name: 'CrmIndex',
-  components: {},
-  // filters: {},
-  props: {},
-  // provide() {
-  //   return {}
-  // },
+  name: 'Crm',
   data() {
     return {}
-  },
-  // inject: [],
-  computed: {},
-  // watch: {},
-  mounted() {
-  },
-  // beforeCreate() {
-  // },
-  // created() {
-  // },
-  // beforeMount() {
-  // },
-  // beforeUpdate() {
-  // },
-  // updated() {
-  // },
-  // beforeDestroy() {
-  // },
-  // destroyed() {
-  // },
-  // activated() {
-  // },
-  // deactivated() {
-  // },
-  methods: {}
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 #biaoti {
   font-size: 40px;
-  font-weight: bold;
+  font-weight: bolder;
   color: #1b2559;
   text-align: center;
   width: 100%;
-  height: 150px;
   line-height: 100px
 }
-
-.gongneng {
+.gongneng{
   background-color: blue;
   width: 27%;
   float: left;
@@ -129,49 +89,31 @@ export default {
   font-size: 20px;
   letter-spacing: 7px;
 }
-
-.zuofu {
-  float: left;
+.zuofu{
   width: 140px;
   height: 140px;
   text-align: center;
   box-shadow: 3px 3px 10px 0px #bbd0e5;
   line-height: 140px;
   color: #1b2559;
-  padding: 23%;
+  padding: 30%;
 }
-
-.youbian {
-  height: 10%;
-  line-height: 140px;
+.youbian{
+  height: 140px;
+  line-height: 85px;
   text-align: center;
   color: #a9a4a4;
   font-size: 28px;
-  float: left;
   font-weight: bolder;
 }
-
-.shangfang {
-  width: 80%;
-  float: left;
-  height: 160px;
-}
-
-.gezi {
-  float: left;
-  width: 17.8%;
-}
-
-.wenan {
+.wenan{
   width: 140px;
   padding-top: 30px;
   color: gray;
   text-align: center;
   letter-spacing: 2px;
-  float: left;
 }
-
-.wenan2 {
+.wenan2{
   width: 100%;
   height: 100%;
   font-size: 23px;
@@ -180,8 +122,7 @@ export default {
   line-height: 100%;
   margin: 0px;
 }
-
-.footwenan {
+.footwenan{
   line-height: 60px;
   box-shadow: rgb(187 208 229) 3px 3px 10px 0px;
   height: 60px;
@@ -192,4 +133,11 @@ export default {
   text-align: center;
   margin: 1% 5%;
 }
+/*.bigtd{*/
+/*  width: 17%;*/
+/*}*/
+.smalltd{
+  width: 10%;
+}
+
 </style>
